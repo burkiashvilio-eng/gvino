3c82d35cea5e260862be241d0ff67139ce031906
burkiashvilio-eng <burkiashvilio@gmail.com>
2026-05-16 03:00:58 +0400

    Update LogoCircle.tsx

--- components/LogoCircle.tsx
+++ components/LogoCircle.tsx
@@ -3,7 +3,7 @@ export default function LogoCircle() {
     <div className="logo-circle-wrap">
       <div className="logo-circle">
         {/* eslint-disable-next-line @next/next/no-img-element */}
-        <img src="/logo.jpg" alt="Burkiashvili Wine Cellar" />
+        <img src="/Logo.jpeg" alt="Burkiashvili Wine Cellar" />
       </div>
       <div className="brand-name">Burkiashvili Wine Cellar</div>
     </div>
